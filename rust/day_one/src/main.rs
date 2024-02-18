e2c7ce75703100dc100c43c0b4f922262293c41e
Tonitum <cameron@tonitum.com>
2024-02-17 20:51:56 -0700

    refs year-2023: got the test input working.

--- rust/day_one/src/main.rs
+++ rust/day_one/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, ops::Add};
 
 fn load_input(filename: String) -> String {
     let contents = fs::read_to_string(filename).
@@ -13,33 +13,26 @@ fn split_input_into_array(lines: &String)  -> Vec<&str>{
 }
 
 fn get_first_and_last_digit(input_line: &str) -> u32 {
-    let mut first_digit: char;
-    let mut second_digit: char;
+    let mut first_digit: u32 = 0;
+    let mut second_digit: u32 = 0;
     for char in input_line.chars().rev() {
        if !char.is_digit(10) {
            continue;
        }
-       first_digit = char;
+       first_digit = char.to_digit(10).unwrap();
     }
     for char in input_line.chars() {
        if !char.is_digit(10) {
            continue;
        }
-       second_digit = char;
+       second_digit = char.to_digit(10).unwrap();
     }
-    (first_digit+second_digit)
-    // need to combine the chars and then convert to integer
-    // need to also chyage the vector to a single u32
+    let number_string = first_digit.to_string().add(&second_digit.to_string());
+    dbg!(&number_string);
+    let number = number_string.parse::<u32>().unwrap();
+    number
 }
 
-fn sum_digit_pairs(pairs: &Vec<(u32,u32)>) -> u32 {
-    let mut sum = 0;
-    for pair in pairs {
-        let pair_sum = pair.0 + pair.1;
-        sum += pair_sum;
-    }
-    sum
-}
 
 fn main() {
     // load the input file
@@ -48,12 +41,10 @@ fn main() {
     // split into lines
     let split_lines = split_input_into_array(&input_lines);
     // for each line, get the first digit and last digit
-    let mut pairs: Vec<(u32,u32)> = Vec::new();
+    let mut total_sum: u32 = 0;
 
     for line in split_lines {
-        pairs.push(get_first_and_last_digit(line));
-        dbg!(get_first_and_last_digit(line));
+        total_sum = total_sum + get_first_and_last_digit(line);
     }
-    let total_sum = sum_digit_pairs(&pairs);
     dbg!(&total_sum);
 }
